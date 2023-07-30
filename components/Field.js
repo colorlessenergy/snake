00fecbdc0a4a53589c8edd7b65f71f4171475351
colorlessenergy <brianmunozdev@gmail.com>
2023-07-30 07:35:53 -0700

    add functionality to generate grid

--- components/Field.js
+++ components/Field.js
@@ -1,5 +1,44 @@
 const Field = () => {
-    return <div className="field"></div>;
+    const generateGrid = (cols, rows) => {
+        let grid = [];
+
+        for (let i = 0; i < cols; i++) {
+            grid.push([]);
+
+            for (let j = 0; j < rows; j++) {
+                grid[i].push({
+                    x: i * 50,
+                    y: j * 50,
+                    hasFood: false,
+                    hasSnake: false
+                });
+            }
+        }
+
+        return grid;
+    };
+
+    const grid = generateGrid(14, 7);
+
+    return (
+        <div className="field">
+            {grid.map(col => {
+                return col.map(square => {
+                    return (
+                        <div
+                            style={{
+                                border: '1px solid #ffffff',
+                                height: '50px',
+                                width: '50px',
+                                position: 'absolute',
+                                top: square.y,
+                                left: square.x
+                            }}></div>
+                    );
+                });
+            })}
+        </div>
+    );
 };
 
 export default Field;
